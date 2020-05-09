5a4a1b016613d1c4d5b8ccab0c569b2f1b378a7f
Rafał Major <rmaj@dgs.com>
2020-05-09 10:01:15 +0200

    Scale bottom menu

--- TomatoTimer/BottomMenuViewModel.cs
+++ TomatoTimer/BottomMenuViewModel.cs
@@ -1,7 +1,24 @@
-﻿namespace TomatoTimer
+﻿using System.Diagnostics;
+using System.Windows.Input;
+
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace TomatoTimer
 {
-    public class BottomMenuViewModel
+    public class BottomMenuViewModel : BindableBase
     {
-        
+        private ICommand startCommand;
+
+        private ICommand stopCommand;
+
+        private ICommand breakCommand;
+
+
+        public ICommand StartCommand => this.startCommand ??= new DelegateCommand(() => {});
+
+        public ICommand StopCommand => this.stopCommand ??= new DelegateCommand(() => {});
+
+        public ICommand BreakCommand => this.breakCommand ??= new DelegateCommand(() => {});
     }
 }
