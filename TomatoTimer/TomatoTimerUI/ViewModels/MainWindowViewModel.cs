07ecd6466eceb9c92dd5213bddb1338d8ad42528
Rafał Major <rmaj@dgs.com>
2021-02-02 22:34:41 +0100

    Empty project. Add mock clock and button plus icon.

--- TomatoTimer/TomatoTimerUI/ViewModels/MainWindowViewModel.cs
+++ TomatoTimer/TomatoTimerUI/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,36 @@
-﻿using Prism.Mvvm;
+﻿using System.Windows.Input;
+using Microsoft.Xaml.Behaviors.Core;
+using Prism.Mvvm;
 
 namespace TomatoTimerUI.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
-        public string Title
+        private string currentTime = "00:00";
+        private bool isStarted;
+        private ICommand setBreakCommand;
+        private ICommand setTomatoCommand;
+        private ICommand startCommand;
+        private ICommand stopCommand;
+
+        public string CurrentTime
         {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
+            get => this.currentTime;
+            set => this.SetProperty(ref this.currentTime, value);
         }
 
-        public MainWindowViewModel()
+        public bool IsStarted
         {
-
+            get => this.isStarted;
+            set => this.SetProperty(ref this.isStarted, value);
         }
+
+        public ICommand StartCommand => this.startCommand ??= new ActionCommand(() => { this.IsStarted = true; });
+
+        public ICommand StopCommand => this.stopCommand ??= new ActionCommand(() => { this.IsStarted = false; });
+
+        public ICommand SetTomatoCommand => this.setTomatoCommand ??= new ActionCommand(() => { });
+
+        public ICommand SetBreakCommand => this.setBreakCommand ??= new ActionCommand(() => { });
     }
 }
